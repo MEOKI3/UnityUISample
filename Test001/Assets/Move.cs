bf9b4cc58b2b56581183e985d7bb6f51d4d239d0
MEOKI3 <gooder0317@gmail.com>
2023-08-22 21:06:39 +0900

    Test1 Upload

---
+++ Test001/Assets/Move.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] bool isUI;
+    private void Update()
+    {
+        if(isUI == true)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+            transform.Translate(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0);
+        }
+        else if(isUI == false)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            transform.Translate(x * Time.deltaTime * speed, 0, z * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.R)) transform.Rotate(0, 0, 10 * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.T)) transform.Rotate(0, 10 * Time.deltaTime * speed, 0);
+
+        if (Input.GetKey(KeyCode.Y)) transform.Rotate(10 * Time.deltaTime * speed, 0, 0);
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            Vector3 scale = transform.localScale;
+            scale += new Vector3(0.01f, 0.01f, 0.01f);
+            transform.localScale = scale;
+        }
+
+        if (Input.GetKey(KeyCode.V))
+        {
+            Vector3 scale = transform.localScale;
+            scale -= new Vector3(0.01f, 0.01f, 0.01f);
+            transform.localScale = scale;
+        }
+    }
+}
